8f75c8a08c79fd522c589bcc00e834cdb29b17e2
Argk-GitHub <90384221+Argk-GitHub@users.noreply.github.com>
2021-09-10 19:00:24 +0200

    Bot sends ONLY ambeds

--- bot.py
+++ bot.py
@@ -1,25 +1,15 @@
 from discord.ext import commands
-import discord, time
+import discord, time, os, sys
 from discord.ext.commands import has_permissions
-import threading
 
 prefix = "."
 bot = commands.Bot(command_prefix=prefix)
-bot.remove_command("help")
 
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Game(name="Minecraft"))
     print("erfolgreich gestartet")
     
-
-
-
-
-
-
-
-
 @bot.event
 async def on_message(ctx):
 
@@ -27,7 +17,7 @@ async def on_message(ctx):
     await bot.process_commands(ctx)
 
 
-@bot.command()
+@bot.command(description="kickt einen Member")
 async def kick(ctx, member: discord.Member, *, reason):
     if ctx.author.permissions_in(ctx.channel).kick_members:
         await member.kick(reason=reason)
@@ -38,51 +28,56 @@ async def kick(ctx, member: discord.Member, *, reason):
                                 color=0x22a7f0)
         await ctx.send(embed=embed)
     else:
-        embed = discord.Embed(title=f'ERROR:',
-                                description=f"You can't use this command {ctx.author.name}.",
-                               color=0x22a7f0)
+        embed=discord.Embed(title="Error", description="Du hast keine Permission ,diesen Command auszuführen", color=0xff6600)
         await ctx.send(embed=embed)
 
-
-@bot.command()
-async def help(ctx):
-    embed = discord.Embed(title=f'**Du brauchst Hilfe **{ctx.author.name}?',
-                            description=f'Falls du eine Liste der Commands willst , gibt /commands ein.\n'
-                                        f'\n'
-                                        f'Du möchtest Metronom auf deinem Server??\n'
-                                        f"**Invite Link**: none",
-                            color=0x22a7f0)
-    await ctx.send(embed=embed)
-
-@bot.command()
+@bot.command(despription="Zeigt dir befehle für Moderator/Höher")
 async def modhelp(ctx):
     embed = discord.Embed(title=f'**Du brauchst hilfe **{ctx.author.name}?',
                             description=f'**Diese Befehle können nur von mods und höher ausgefführt werden**\n'
                                         f'**ban [member]**-bannt einen Member\n'
                                         f'**kick [member]**-kickt einen Member\n'
-                                        f"**shutdown**-Fährt den Bot herunter\n"
                                         f'**clear [anzahl]**-löscht Nachrichten\n'
                                         f"**unban [member mit tag]**-Entbannt einen Member",
                             color=0x22a7f0)
     await ctx.send(embed=embed)
     
 
-@bot.command()
+@bot.command(discription="entbannt einen member")
 async def unban(ctx, *, member):
     if ctx.author.permissions_in(ctx.channel).ban_members:
         banned_users = await ctx.guild.bans()
         member_name, member_discriminator = member.split('#')
+        
+    else:
+        embed=discord.Embed(title="Error", description="Du hast keine Permission ,diesen Command auszuführen", color=0xff6600)
+        await ctx.send(embed=embed)
 
     for ban_entry in banned_users:
         user = ban_entry.user
+
+    
   
     if (user.name, user.discriminator) == (member_name, member_discriminator):
         await ctx.guild.unban(user)
-        await ctx.send(f"{user} have been unbanned sucessfully")
-            
+        embed=discord.Embed(title="**entbannt**", description=f"Du hast {member} entbannt", color=0xff6600)
+        await ctx.send(embed=embed)
 
 
 
+@bot.command(description=f"Dieser Command restartet den Bot, kann nur von dem Bot-Owner ausgeführt werden.")
+async def restart(ctx):
+    if ctx.author.id == 870241231555399692:
+        embed=discord.Embed(title="**Restartet**", description=f"Der Bot wurde erfolgreich restartet", color=0xff6600)
+        await ctx.send(embed=embed)
+
+        os.execv(sys.executable, ['python'] + sys.argv)
+        
+
+    else:
+        embed=discord.Embed(title="Error", description="Du hast keine Permission ,diesen Command auszuführen", color=0xff6600)
+        await ctx.send(embed=embed)
+    
  
  
 
@@ -90,37 +85,27 @@ async def unban(ctx, *, member):
 async def ban(ctx, member : discord.Member, *, reason=None):
     if ctx.author.permissions_in(ctx.channel).ban_members:
         await member.ban(reason=reason)
-        await ctx.send(f'banned {member.mention} from the server')
+        embed=discord.Embed(title="**Banned**", description=f"Du hast {member} gebannt", color=0xff6600)
+        await ctx.send(embed=embed)
+    else:
+        embed=discord.Embed(title="Error", description="Du hast keine Permission ,diesen Command auszuführen", color=0xff6600)
+        await ctx.send(embed=embed)
 
 
 
-@bot.command()
-async def commands(ctx):
-    embed = discord.Embed(title=f'**Do you need help **{ctx.author.name}?',
-                            description=f'**Der Prefix ist /**\n'
-                                        f'**selfinfo**-Zeigt dir die Info von deinem Discord-Account an\n'
-                                        f'**userinfo [pinge den User hier]**-Zeigt dir die Info des gepingtenUsers an\n'
-                                        f"**msg [userid] [nachricht]**-sendet dem user die Message\n"
-                                        f'**botinfo**-Gibt dir Daten über dem Bot\n'
-                                        f'**patchnotes**-Zeigt dir die neuen Funktionen des Bots\n'
-                                        f'**ping**-Zeigt dir die Verbindung zum Bot\n'                            
-                                        f'**echo** [nachricht]-Wiederholt die gegebene Nachricht\n'
-                                        f"**support** -Supportet den Bot!",
-                            color=0x990000)
-    await ctx.send(embed=embed)
 
 
 
 @bot.command(pass_context=True, description="schreibt eine message per dm an einen Member")
-async def msg(ctx, userid: str, *, msg):
-    user = await bot.fetch_user(userid)
-    await user.send(f"{msg}")
+async def msg(ctx,  member : discord.Member, *, msg):
+    user = await bot.fetch_user(member.id)
+    await user.send(msg)
     await ctx.channel.purge(limit=1)
 
 
 
 
-@bot.command(description="Zeigt den ping!")
+@bot.command(description="Zeigt den ping zum bot!")
 async def ping(ctx):
     if round(bot.latency * 1000) <= 50:
             embed=discord.Embed(title="PING", description=f":ping_pong: The ping is **{round(bot.latency *1000)}** milliseconds!", color=0x44ff44)
@@ -144,30 +129,18 @@ async def clear(ctx, amount=2):
     if ctx.author.permissions_in(ctx.channel).kick_members:
         await ctx.channel.purge(limit=amount+1)
         return
+    else:
+        embed=discord.Embed(title="Error", description="Du hast keine Permission ,diesen Command auszuführen", color=0xff6600)
+        await ctx.send(embed=embed)
 
 @bot.event
 async def on_command_error(ctx, error):
-    print(error)
-    await ctx.send("Sorry , Befehl nicht gefunden")
-    await ctx.send("Hast du die Groß-Und Kleinschreibung richtig gemacht?")
-    await ctx.send("Hast du Permissions, das zu tun?")
-    await ctx.send("/help für alle Befehle")
+    embed = discord.Embed(title=f'**Do you need help **{ctx.author.name}?',
+                            description=f"**Befehl nicht gefunden \n"
+                            f"**Hast du dich vertippt?\n"
+                            f"**Wenn du Hilfe brauchst, dann schreib .help! \n",
+                            color=0x990000)
+    await ctx.send(embed=embed)
 
-@bot.command()
-async def support(ctx):
-    embedAnswer = discord.Embed(color=0x22a7f0)
-    embedAnswer.add_field(name='Support', value='React with ✅ to support us!')
-    embedAnswer.set_footer(text='- Support')
-    msg = await ctx.send(embed=embedAnswer)
-    await msg.add_reaction('✅')
-    del embedAnswer
-    channel = await bot.fetch_channel(846797857880408175)
 
-    await channel.send(f"{ctx.author.name} supportet den sheeshBot")
-    embed = discord.Embed(title=f"You wan't to support our projekt?",
-                            description=f'If you would like to support our project, you are welcome to create\n'
-                                        f'a ticket.\n'
-                                        f'Just write "#support" in the first line as a distinguishing mark.',
-                            color=0x22a7f0)
-
-bot.run("TOKEN")
+bot.run("NEIN")
